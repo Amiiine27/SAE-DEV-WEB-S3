adcfa71c007d9e04a5364ccd6c572c4b10c05a9c
Sagbyy <sbouhdjeur.gagny@gmail.com>
2023-12-13 14:49:42 +0100

    Règlage de quelques style

--- src/template.php
+++ src/template.php
@@ -1,46 +1,49 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../style/utilities.css">
     <link rel="stylesheet" href="../style/style.css">
-    <link href="https://fonts.googleapis.com/css2?family=Krona+One&family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300&display=swap" rel="stylesheet">    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Krona+One&family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300&display=swap"
+          rel="stylesheet">
+    <title>Document</title>
 </head>
 
 <body>
-    <header class="entete bg-color">
-        <nav class="navbar">
-            <a class="left" href="index.php"><img src="../images/logo.png" alt="Logo du site web"></a>
-            <ul class="mid">
-                <li class="liste"><a href="#">Accueil</a></li>
-                <li class="liste"><a href="#">Map</a></li>
-                <li class="liste"><a href="#">Shop</a></li>
-                <li class="liste"><a href="#">Equipage</a></li>
-                <li class="liste"><a href="#">Encyclopédie</a></li>
-                <li class="liste"><a href="#">Accueil</a></li>
-            </ul>
-            <!-- Modal -->
-            <div id="myModal" class="modal">
-                <div class="modal-content">
-                    <span class="close" onclick="closeModal()">&times;</span>
-                    <!-- Contenu du modal -->
-                    <h1>Connexion</h1>
-                </div>
+<header class="entete bg-color">
+    <nav class="navigation-bar">
+        <a class="left" href="index.php"><img src="../images/logo.png" alt="Logo du site web"></a>
+        <ul class="mid">
+            <li class="liste"><a href="#">Accueil</a></li>
+            <li class="liste"><a href="#">Map</a></li>
+            <li class="liste"><a href="#">Shop</a></li>
+            <li class="liste"><a href="#">Equipage</a></li>
+            <li class="liste"><a href="#">Encyclopédie</a></li>
+            <li class="liste"><a href="#">Accueil</a></li>
+        </ul>
+        <!-- Modal -->
+        <div id="myModal" class="modal">
+            <div class="modal-content">
+                <span class="close" onclick="closeModal()">&times;</span>
+                <!-- Contenu du modal -->
+                <h1>Connexion</h1>
             </div>
-            
-            <!-- Balise "Se connecter" -->
-            <div class="right">
-                <a href="#" class="open-modal"><img src="../images/connexion-logo.svg" alt="Connexion">Se connecter</a>
-            </div>
-        </nav>
-    </header>
+        </div>
+
+        <!-- Balise "Se connecter" -->
+        <div class="right">
+            <a href="#" class="open-modal"><img src="../images/connexion-logo.svg" alt="Connexion">Se connecter</a>
+        </div>
+    </nav>
+</header>
 
 
-    <footer>
-        <p>© 2023 Red Line Defense, Inc. Tous droits réservés.</p>
-    </footer>
+<footer>
+    <p>© 2023 Red Line Defense, Inc. Tous droits réservés.</p>
+</footer>
 </body>
 
 <script>
@@ -72,5 +75,7 @@
     });
 
 </script>
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
 </html>
